53e74f771c4b879e90f6ea98014cc4486001d975
Gamer_Z <grasmanek94@gmail.com>
2017-10-27 01:30:11 +0200

    Sync

--- Assets/Scripts/PropManager.cs
+++ Assets/Scripts/PropManager.cs
@@ -4,13 +4,161 @@ using UnityEngine;
 
 public class PropManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public enum PropType
+    {
+        Bench_A,
+        Bench_B,
+        Bench_C,
+        FuseBox,
+        Hidrant,
+        MailBox,
+        NewsBoard,
+        ParkLamp,
+        Phone,
+        PlantPot_A_V01,
+        PlantPot_A_V02,
+        PlantPot_A_V03,
+        PlantPot_A_V04,
+        PlantPot_A_V05,
+        PlantPot_A_V06,
+        PlantPot_B_V01,
+        PlantPot_B_V02,
+        PlantPot_B_V03,
+        PlantPot_C_V01,
+        PlantPot_C_V02,
+        PlantPot_C_V03,
+        PlantPot_D_V01,
+        PlantPot_D_V02,
+        PlantPot_D_V03
+    }
+
+    public enum BlockType
+    {
+        RoadBlock_A,
+        RoadBlock_B,
+        RoadBlock_C,
+        RoadBlock_D
+    }
+
+    private class InstanceManager<T>
+    {
+        private Dictionary<T, GameObject> instances;
+        private List<T> t_types;
+        private List<GameObject> storage_instance;
+        private HashSet<GameObject> storage_check;
+
+        public InstanceManager()
+        {
+            storage_check = new HashSet<GameObject>();
+            storage_instance = new List<GameObject>();
+            instances = new Dictionary<T, GameObject>();
+            t_types = new List<T>();
+        }
+
+        public void Instantiate(T type, string resource)
+        {
+            if(!t_types.Contains(type))
+            {
+                t_types.Add(type);
+            }
+            instances.Add(type, (GameObject)Resources.Load(resource));
+        }
+
+        public GameObject GetRandom()
+        {
+            GameObject game_object;
+
+            if (storage_instance.Count < 1)
+            {
+                T type = t_types[Random.Range(0, t_types.Count)];
+                game_object = Object.Instantiate(instances[type]);
+                return game_object;
+            }
+
+            int where = Random.Range(0, storage_instance.Count);
+            game_object = storage_instance[where];
+            storage_instance.RemoveAt(where);
+            storage_check.Remove(game_object);
+
+            return game_object;
+        }
+
+        public void Return(GameObject game_object)
+        {
+            if (storage_check.Contains(game_object))
+            {
+                return;
+            }
+
+            storage_instance.Add(game_object);
+        }
+    }
+
+    private static PropManager instance = null;
+    private static InstanceManager<PropType> props;
+    private static InstanceManager<BlockType> blocks;
+
+    public static GameObject GetRandomProp()
+    {
+        return props.GetRandom();
+    }
+
+    public static void ReturnProp(GameObject game_object)
+    {
+        props.Return(game_object);
+        game_object.transform.parent = instance.transform;
+    }
+
+    public static GameObject GetRandomBlock()
+    {
+        return blocks.GetRandom();
+    }
+
+    public static void ReturnBlock(GameObject game_object)
+    {
+        blocks.Return(game_object);
+        game_object.transform.parent = instance.transform;
+    }
+
+    private void Awake()
+    {        
+        if(instance == null)
+        {
+            instance = this;
+
+            props = new InstanceManager<PropType>();
+
+            props.Instantiate(PropType.Bench_A       , "LowpolyStreetPack/Prefabs/StreetProps/Bench/Bench_A"                      );
+            props.Instantiate(PropType.Bench_B       , "LowpolyStreetPack/Prefabs/StreetProps/Bench/Bench_B"                      );
+            props.Instantiate(PropType.Bench_C       , "LowpolyStreetPack/Prefabs/StreetProps/Bench/Bench_C"                      );
+            props.Instantiate(PropType.FuseBox       , "LowpolyStreetPack/Prefabs/StreetProps/FuseBox/FuseBox"                    );
+            props.Instantiate(PropType.Hidrant       , "LowpolyStreetPack/Prefabs/StreetProps/Hidrant/Hidrant"                    );
+            props.Instantiate(PropType.MailBox       , "LowpolyStreetPack/Prefabs/StreetProps/MailBox/MailBox"                    );
+            props.Instantiate(PropType.NewsBoard     , "LowpolyStreetPack/Prefabs/StreetProps/NewsBoard/NewsBoard"                );
+            props.Instantiate(PropType.ParkLamp      , "LowpolyStreetPack/Prefabs/StreetProps/ParkLamp/ParkLamp"                  );
+            props.Instantiate(PropType.Phone         , "LowpolyStreetPack/Prefabs/StreetProps/Phone/Phone"                        );
+            props.Instantiate(PropType.PlantPot_A_V01, "LowpolyStreetPack/Prefabs/StreetProps/PlantPots/QuickBuild/PlantPot_A_V01");
+            props.Instantiate(PropType.PlantPot_A_V02, "LowpolyStreetPack/Prefabs/StreetProps/PlantPots/QuickBuild/PlantPot_A_V02");
+            props.Instantiate(PropType.PlantPot_A_V03, "LowpolyStreetPack/Prefabs/StreetProps/PlantPots/QuickBuild/PlantPot_A_V03");
+            props.Instantiate(PropType.PlantPot_A_V04, "LowpolyStreetPack/Prefabs/StreetProps/PlantPots/QuickBuild/PlantPot_A_V04");
+            props.Instantiate(PropType.PlantPot_A_V05, "LowpolyStreetPack/Prefabs/StreetProps/PlantPots/QuickBuild/PlantPot_A_V06");
+            props.Instantiate(PropType.PlantPot_A_V06, "LowpolyStreetPack/Prefabs/StreetProps/PlantPots/QuickBuild/PlantPot_A_V06");
+            props.Instantiate(PropType.PlantPot_B_V01, "LowpolyStreetPack/Prefabs/StreetProps/PlantPots/QuickBuild/PlantPot_B_V01");
+            props.Instantiate(PropType.PlantPot_B_V02, "LowpolyStreetPack/Prefabs/StreetProps/PlantPots/QuickBuild/PlantPot_B_V02");
+            props.Instantiate(PropType.PlantPot_B_V03, "LowpolyStreetPack/Prefabs/StreetProps/PlantPots/QuickBuild/PlantPot_B_V03");
+            props.Instantiate(PropType.PlantPot_C_V01, "LowpolyStreetPack/Prefabs/StreetProps/PlantPots/QuickBuild/PlantPot_C_V01");
+            props.Instantiate(PropType.PlantPot_C_V02, "LowpolyStreetPack/Prefabs/StreetProps/PlantPots/QuickBuild/PlantPot_C_V02");
+            props.Instantiate(PropType.PlantPot_C_V03, "LowpolyStreetPack/Prefabs/StreetProps/PlantPots/QuickBuild/PlantPot_C_V03");
+            props.Instantiate(PropType.PlantPot_D_V01, "LowpolyStreetPack/Prefabs/StreetProps/PlantPots/QuickBuild/PlantPot_D_V01");
+            props.Instantiate(PropType.PlantPot_D_V02, "LowpolyStreetPack/Prefabs/StreetProps/PlantPots/QuickBuild/PlantPot_D_V02");
+            props.Instantiate(PropType.PlantPot_D_V03, "LowpolyStreetPack/Prefabs/StreetProps/PlantPots/QuickBuild/PlantPot_D_V03");
+
+            blocks = new InstanceManager<BlockType>();
+
+            blocks.Instantiate(BlockType.RoadBlock_A, "LowpolyStreetPack/Prefabs/StreetProps/RoadBlocks/RoadBlock_A");
+            blocks.Instantiate(BlockType.RoadBlock_B, "LowpolyStreetPack/Prefabs/StreetProps/RoadBlocks/RoadBlock_B");
+            blocks.Instantiate(BlockType.RoadBlock_C, "LowpolyStreetPack/Prefabs/StreetProps/RoadBlocks/RoadBlock_C");
+            blocks.Instantiate(BlockType.RoadBlock_D, "LowpolyStreetPack/Prefabs/StreetProps/RoadBlocks/RoadBlock_D");
+        }
+    }
 }
---
+++ Assets/Scripts/PropPlacer.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropPlacer : MonoBehaviour {
+
+    [System.Serializable]
+
+    public class SimpleTransform
+    {
+        public Vector3 position;
+        public Vector3 rotation;
+    }
+    
+    [System.Serializable]
+    public class ProbableOffset
+    {
+        public float probability;
+        public SimpleTransform[] offsets;
+    }
+
+    [System.Serializable]
+    public class PropPoint
+    {
+        public ProbableOffset[] probable_offsets;
+
+        private float probability_sum = 0.0f;
+        public void CalculateProbability()
+        {
+            foreach (ProbableOffset probable_offset in probable_offsets)
+            {
+                probability_sum += probable_offset.probability;
+            }
+
+            if (probability_sum < 1.0f)
+            {
+                probability_sum = 1.0f;
+            }
+        }
+
+        public ProbableOffset GetByProbability()
+        {
+            float r = Random.Range(0.0f, 0.99999999f) * probability_sum;
+
+            double sum = 0;
+            foreach (ProbableOffset n in probable_offsets)
+            {
+                // loop until the random number is less than our cumulative probability
+                if (r <= (sum = sum + n.probability))
+                {
+                    return n;
+                }
+            }
+
+            return null;
+        }
+    }
+
+    public PropPoint[] prop_points;
+    public PropPoint[] block_points;
+
+    private List<GameObject> attached_props;
+    private List<GameObject> attached_blocks;
+
+    private void Awake()
+    {
+        attached_props = new List<GameObject>();
+        attached_blocks = new List<GameObject>();
+
+        foreach (PropPoint point in prop_points)
+        {
+            point.CalculateProbability();
+        }
+
+        foreach (PropPoint point in block_points)
+        {
+            point.CalculateProbability();
+        }
+    }
+
+    private void OnEnable()
+    {
+        foreach (PropPoint point in prop_points)
+        {
+            ProbableOffset po = point.GetByProbability();
+            if (po != null)
+            {
+                foreach (SimpleTransform offset in po.offsets)
+                {
+                    GameObject game_object = PropManager.GetRandomProp();
+                    attached_props.Add(game_object);
+                    game_object.transform.parent = transform;
+                    game_object.transform.position = offset.position;
+                    game_object.transform.rotation = Quaternion.Euler(offset.rotation);
+                }
+            }
+        }
+
+        foreach (PropPoint point in block_points)
+        {
+            ProbableOffset po = point.GetByProbability();
+            if (po != null)
+            {
+                foreach (SimpleTransform offset in po.offsets)
+                {
+                    GameObject game_object = PropManager.GetRandomBlock();
+                    attached_blocks.Add(game_object);
+                    game_object.transform.parent = transform;
+                    game_object.transform.position = offset.position;
+                    game_object.transform.rotation = Quaternion.Euler(offset.rotation);
+                }
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach(GameObject game_object in attached_props)
+        {
+            PropManager.ReturnProp(game_object);
+        }
+        attached_props.Clear();
+
+        foreach (GameObject game_object in attached_blocks)
+        {
+            PropManager.ReturnBlock(game_object);
+        }
+        attached_blocks.Clear();
+    }
+}
