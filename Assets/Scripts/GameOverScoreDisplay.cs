b7feabfdf35d68a8f765cda595813a0cc1f1567b
Gamer_Z <grasmanek94@gmail.com>
2017-10-26 19:59:19 +0200

    Sync

--- Assets/Scripts/GameOverScoreDisplay.cs
+++ Assets/Scripts/GameOverScoreDisplay.cs
@@ -7,6 +7,14 @@ public class GameOverScoreDisplay : MonoBehaviour {
 	// Use this for initialization
 	void OnEnable () {
         Text text = GetComponent<Text>();
-        text.text = "Score\r\n" + ((int)SharedObject.Get<float>("score")).ToString();
+        int current_score = (int)SharedObject.Get<float>("score");
+        text.text = "Score\r\n" + current_score.ToString();
+
+        int saved_score = PlayerPrefs.GetInt("score");
+        if(saved_score < current_score)
+        {
+            PlayerPrefs.SetInt("score", current_score);
+            PlayerPrefs.Save();
+        }
 	}
 }
